b3f90255d65ebc7b713d9682c57da60dee8f1e5c
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-20 02:35:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,10 @@
 def using_include(array, element)
   array.include?(element)
 end
-
 p using_include([1, 2, 3, 4, 5], 5)
+
+def using_sort(array) 
+  sorted_array
+  sorted_array = array.sort
+
+end p using_sort(["John", "Maggie", "Bogusia"])
